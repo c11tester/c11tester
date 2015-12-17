3897cb4ac31fe710f2333bd6ca917144b58844f2
bdemsky <bdemsky@uci.edu>
2015-12-17 00:37:52 -0800

    Increase size of bootstrap bytes as some Linux distributions need more space.

--- mymemory.cc
+++ mymemory.cc
@@ -124,7 +124,7 @@ void model_free(void *ptr)
 /** Bootstrap allocation. Problem is that the dynamic linker calls require
  *  calloc to work and calloc requires the dynamic linker to work. */
 
-#define BOOTSTRAPBYTES 4096
+#define BOOTSTRAPBYTES 131072
 char bootstrapmemory[BOOTSTRAPBYTES];
 size_t offset = 0;
 
@@ -134,7 +134,7 @@ void * HandleEarlyAllocationRequest(size_t sz)
 	sz = (sz + 7) & ~7;
 
 	if (sz > (BOOTSTRAPBYTES-offset)) {
-		model_print("OUT OF BOOTSTRAP MEMORY\n");
+		model_print("OUT OF BOOTSTRAP MEMORY.  Increase the size of BOOTSTRAPBYTES in mymemory.cc\n");
 		exit(EXIT_FAILURE);
 	}
 
