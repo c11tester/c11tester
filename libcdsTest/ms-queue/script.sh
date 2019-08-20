c740f6dcf34a9afd8e3bf7a9c857cfdbc27420c8
bdemsky <bdemsky@uci.edu>
2019-08-20 14:21:46 -0700

    Remove msqueue test case

--- libcdsTest/ms-queue/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-clang++ -o msqueue2 -Xclang -load -Xclang /scratch/llvm/build/lib/libCDSPass.so intrusive_msqueue_hp.cc -I. -I /scratch/benchmarks/libcds -g -O1 -std=c++11 -L /scratch/new-fuzzer -lmodel -L /scratch/benchmarks/libcds/libcds-llvm/bin -lcds_d -lpthread
-
-export LD_LIBRARY_PATH=/scratch/benchmarks/libcds/libcds-llvm/bin:/scratch/new-fuzzer/
